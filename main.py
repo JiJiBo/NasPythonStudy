225f1cb4ca149c39d5afcf96e407d860930f91a8
NasNeo <1270071138@qq.com>
2025-09-01 19:50:32 +0800

    初始化

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from src.ui.splash_page import splash_page
 
 def main(page: ft.Page):
     page.window_icon = "icon.png"
+    page.theme = ft.Theme(font_family="Microsoft YaHei")  # 或者 "Microsoft YaHei", "Noto Sans", "PingFang SC"
     page.bgcolor = ft.Colors.WHITE
     splash_page(page)
 
